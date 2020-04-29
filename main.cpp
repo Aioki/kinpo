c348e6cb45985068132711fa9640c51ce8c5c740
Aioki <aioki@outlook.com>
2020-04-29 16:22:38 +0400

    Создание прототипа, отображение одного элемента

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+//a\u2081\u2082\u2083
+//36pt = 24 пкс//17 пкс высота
+//36pt = 16 пкс//9//12//12 ширина
+
+int main(int argc, char *argv[])
+{
+
+    QGuiApplication app( argc, argv );                      //Инициализация ядра приложения
+
+    QImage   img(70,70,QImage::Format_RGB32);
+    QPainter p(&img);
+    QRect    tmp(0,0,70,70);
+
+    p.setFont(QFont("Times", 36));                          //Установка шрифта
+
+    img.fill(Qt::white);                                    //Установка белого фона
+    //p.fillRect(tmp,QBrush(Qt::white,Qt::SolidPattern));   //Закраска квадрата
+
+    qDebug() << "Set painter";
+
+    p.drawText(tmp, Qt::AlignHCenter, QString("a"+tmp1[9])); //Печать элемента
+    qDebug() << "Draw text";
+
+    p.drawLine(0,0,70,70);                                  //Отрисовка диагональной линии
+    qDebug() << "Draw line";
+
+    img.save("txt.png", "PNG");                             // writes image into ba in PNG format
+    qDebug() << "Success";
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <QObject>
+#include <QGuiApplication>
+#include <QtGui/QImage>
+#include <QtGui/QPainter>
+
+#include "QDebug"
+
+QString tmp1[10] = {"\u2080","\u2081","\u2082","\u2083","\u2084","\u2085","\u2086","\u2087","\u2088","\u2089"};
+
+char subscript_number[10][7]={"\u2080","\u2081","\u2082","\u2083","\u2084","\u2085","\u2086","\u2087","\u2088","\u2089"};
+
+#endif // MAIN_H
