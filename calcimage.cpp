44f5731d73d2bd8489fd91f6f623cf36c8f04d0d
Ivan Gulyaev <aioki@outlook.com>
2020-05-06 13:33:45 +0400

    Закомментирование функции calcSizeImage()

--- calcimage.cpp
+++ calcimage.cpp
@@ -49,22 +49,32 @@ void CalcImage::calcCell()
 
 void CalcImage::calcSizeImage()
 {
+
     //Рассчитать размер ячейки
     calcCell();
-
+    //Если одномерный массив (1хi)
     if (arr_index.size() == 1){
-
+        //Ширина = размер i, умноженного на размер ячейки
         width  = arr_index[0] * size_cell;
+        //Высота = размер ячейки
         height = size_cell;
     }else
+        //Если двумерный массив(ixj)
         if (arr_index.size() == 2 ){
+            //Ширина = размер j, умноженный на размер ячейки
             width  = arr_index[1] * size_cell;
+            //Высота = размер i, умноженный на размер ячейки
             height = arr_index[0] * size_cell;
         }else
+            //Если трехмерный массив(ixjxk)
             if (arr_index.size() == 3) {
+                //Ширина = размер k, умноженный на размер ячейки
                 width  = arr_index[2] * size_cell;
+                //Высота = размер i * j (количество двумерных массивов)
+                // +  отступы между массивами, умноженный на количество двумерных массивов - 1
                 height = arr_index[0]*arr_index[1] * size_cell+(arr_index[0]-1)*size_indent_between_arr;
             }
+    //Добавление отступов слева и справа, сверху и снизу
     width+=(size_indent*2);
     height+=(size_indent*2);
 }
