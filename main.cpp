ceba48c081c7e94d729ead9993a1b1b649c73d37
Ivan Gulyaev <aioki@outlook.com>
2020-05-07 16:10:33 +0400

    Отключение от main.h

--- main.cpp
+++ main.cpp
@@ -9,59 +9,41 @@ int main(int argc, char *argv[])
     QGuiApplication app( argc, argv );                      //Инициализация ядра приложения
 
     int dimension = 3;
-    int size_i    = 1;
-    int size_j    = 2;
+    int size_i    = 2;
+    int size_j    = 4;
     int size_k    = 3;
 
     //Расчет изображения
     CalcImage size_image(dimension,size_i,size_j,size_k);
 
-    image img1(3,4,2,3);
+    qDebug() << size_image.getSizeCell() << " - Cell";
+    qDebug() << size_image.getWidth() << "x" << size_image.getHeight();
 
+    //Ограничение:
+    // Одномерный массив до 100 элементов
+    // Двумерный массив до 100 в каждом направлении
+    // Трехмерный массив 58х1 2х44 19х4 10х8 и (j =90,7/i-1,036 и k до 90)
+    if (size_image.getHeight() >= 16384 || size_image.getWidth() >= 16384){
+        qDebug() << "Over resolution";
+        return -1;
+    }
 
+    //Создание изображение нужного размера
+    image img1(size_image.getHeight(),size_image.getWidth());
+
+    //TODO: Ограничение - изображение не больше
+    img1.drawArrays(dimension,size_i,size_j,size_k,size_image.getSizeCell());
+
+    img1.fillCell(1,1,1);
+
+    img1.fillRow(3,1);
+
+    img1.fillTable(1);
+
+    //img1.drawElements(1,2,3);
+    img1.saveImage();
     //p.fillRect(tmp,QBrush(Qt::white,Qt::SolidPattern));   //Закраска квадрата
 
 
     return 0;
-}
-
-int calcCellForTable(int dimension, int size_i,int size_j, int size_k){
-    //Поиск наибольшего значения
-    QList<int> sort_size;
-    sort_size<<size_i<<size_j<<size_k;
-    qSort(sort_size.begin(),sort_size.end());
-    int max_size = sort_size[2];
-
-    int size_cell = 0;
-    //Расчет производится по формуле:
-    //     отступы по 5 пкс слева и справа
-    //  +  размер названия массива
-    //  +  размер самого большого индекса, умноженного на размерность массива
-    //  +  размер разделителя между индексами, умноженный на размерность - 1
-    size_cell = size_indent+size_name_array+dimension*max_size*size_subscript_number+(dimension-1)*size_comma;
-
-    //Проверка расчета ячейки.
-    if (size_cell ==0) {
-        return -1;
-    } else {
-        return size_cell;
-    }
-}
-
-bool calcSizeImage(int dimension, int size_cell, int *width, int *height)
-{
-    if (dimension == 1){
-        *width  = size_i * size_cell;
-        *height = size_cell;
-    }else
-        if (dimension_ == 2 ){
-            width_img  = size_j_ * size_cell;
-            height_img = size_i_ * size_cell;
-        }else
-            if (dimension_ == 3) {
-                width_img  = size_k_ * size_cell;
-                height_img = size_i_*size_j_ * size_cell+(size_i_-1)*size_indent_between_arr;
-            }
-    width_img+=size_indent*2;
-    height_img+=size_indent*2;
 }
--- main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "image.h"
-#include "calcimage.h"
-
-#endif // MAIN_H
